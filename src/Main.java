5ca199e416d35c3ef0474d8f49c18d1fbe4e0601
Preston Higginbotham <prestonhigg17@gmail.com>
2021-03-26 13:58:47 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        Computer myPhone = new Smartphone(2.55, 4.53);
+        System.out.println("Device has memory: " + myPhone.getMemory() + ", screen area: " +
+                ((Smartphone) myPhone).getScreenWidth() * ((Smartphone) myPhone).getScreenHeight() + " square inches.");
     }
 }
